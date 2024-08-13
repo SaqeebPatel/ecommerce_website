40ea98b75edde84b30deb93cbc18605a406839a9
SaqeebPatel <saqeeb3p@gmail.com>
2024-08-13 22:41:49 +0530

    add the new code on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,13 @@ import "./Sidebar.css";
 const Sidebar = ({ isOpen, toggleSidebar, handleLogout }) => {
   return (
     <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <button  className="btn btn-outline-danger mt-5">
+        Profile
+      </button>
       <button onClick={handleLogout} className="btn btn-outline-danger mt-5">
         Logout
       </button>
+      
     </div>
   );
 };
